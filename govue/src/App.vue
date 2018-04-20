a1b0ae043d2ff621a9818015b716372e1934b855
Mark Ammari <mark.ammari@timeinc.com>
2018-04-20 16:02:30 -0400

    added api from aws

--- govue/src/App.vue
+++ govue/src/App.vue
@@ -1,60 +1,41 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1>{{ title }}</h1>
+      <br>
+        <appBox></appBox>
+      <br>
+      <div class="row">
+        <appUserInput :url="url" @changeUrlbase="changeUrl"></appUserInput>
+      </div>
+    </div>
+    <div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    import UserInput from './components/UserInput.vue';
+    import Box from './components/Box.vue';
+    const urlbase = 'https://19tztqev18.execute-api.us-west-2.amazonaws.com/dev/tag?id=';
+    export default {
+      data: function() {
+        return {
+          title: 'Fetching...',
+          success: '',
+          url: urlbase + 'mark'
+        }
+      },
+      components: {
+          appUserInput: UserInput,
+          appBox: Box,
+      },
+      methods: {
+        changeUrl() {
+          this.url = urlbase + event.target.value
+        }
+      }
     }
-  }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
---
+++ govue/src/components/UserInput.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <button type="button" class="btn btn-primary" @click="checkSuccess">Click</button>
+        <input type="text" placeholder="Value" v-on:keydown.enter="changeUrl">
+        <br></br>
+        <p>{{ url }}</p>
+    </div>    
+</template>
+
+<script>
+    export default {
+        props: ['url'],
+    
+    methods: {
+        checkSuccess(){
+            console.log('I am running')
+            fetch(this.url)
+              .then(response => response.json())
+                .then(myJson => vm.successRate = myJson.success)
+            //       console.log('I am running')
+            // this.$http.get(this.url + '.json').then(response => response.json())
+            // .then(myJson => myJson.Success)
+          },
+        changeUrl() {
+            this.$emit('changeUrlbase', this.url)
+        }  
+    }
+}
+</script>
